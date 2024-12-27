60b26ce609bd70ccd7c0ef2004d8583bc17ddfbf
tjyu1999 <tjyu1999@gmail.com>
2024-12-27 23:28:50 +0800

    edit lab3 exit.c

--- lab3/exit.c
+++ lab3/exit.c
@@ -7,7 +7,6 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/wait.h>
-
 #include <linux/sched.h>
 #include <linux/kernel.h>
 #include <linux/tty.h>
@@ -16,40 +15,40 @@
 int sys_pause(void);
 int sys_close(int fd);
 
-void release(struct task_struct * p)
-{
-	int i;
-
+void release(struct task_struct *p) {
 	if (!p)
 		return;
-	for (i=1 ; i<NR_TASKS ; i++)
-		if (task[i]==p) {
-			task[i]=NULL;
-			free_page((long)p);
+	
+	for (int i = 1 ; i < NR_TASKS ; ++i) {
+		if (task[i] == p) {
+			task[i] = NULL;
+			free_page((long) p);
 			schedule();
+			
 			return;
 		}
+	}
+	
 	panic("trying to release non-existent task");
 }
 
-static inline int send_sig(long sig,struct task_struct * p,int priv)
-{
-	if (!p || sig<1 || sig>32)
+static inline int send_sig(long sig, struct task_struct *p, int priv) {
+	if (!p || sig < 1 || sig > 32)
 		return -EINVAL;
-	if (priv || (current->euid==p->euid) || suser())
-		p->signal |= (1<<(sig-1));
+	if (priv || (current->euid == p->euid) || suser())
+		p->signal |= (1 << (sig - 1));
 	else
 		return -EPERM;
+	
 	return 0;
 }
 
-static void kill_session(void)
-{
+static void kill_session(void) {
 	struct task_struct **p = NR_TASKS + task;
 	
 	while (--p > &FIRST_TASK) {
 		if (*p && (*p)->session == current->session)
-			(*p)->signal |= 1<<(SIGHUP-1);
+			(*p)->signal |= 1 << (SIGHUP - 1);
 	}
 }
 
@@ -57,31 +56,39 @@ static void kill_session(void)
  * XXX need to check permissions needed to send signals to process
  * groups, etc. etc.  kill() permissions semantics are tricky!
  */
-int sys_kill(int pid,int sig)
-{
+int sys_kill(int pid, int sig) {
 	struct task_struct **p = NR_TASKS + task;
-	int err, retval = 0;
+	int err;
+	int retval = 0;
 
-	if (!pid) while (--p > &FIRST_TASK) {
-		if (*p && (*p)->pgrp == current->pid) 
-			if ((err=send_sig(sig,*p,1)))
-				retval = err;
-	} else if (pid>0) while (--p > &FIRST_TASK) {
-		if (*p && (*p)->pid == pid) 
-			if ((err=send_sig(sig,*p,0)))
-				retval = err;
-	} else if (pid == -1) while (--p > &FIRST_TASK) {
+	if (!pid) {
+		while (--p > &FIRST_TASK) {
+		    if (*p && (*p)->pgrp == current->pid) {
+			    if ((err = send_sig(sig, *p, 1)))
+				    retval = err;
+			}
+		}
+	}
+	else if (pid > 0) {
+		while (--p > &FIRST_TASK) {
+		    if (*p && (*p)->pid == pid) {
+			    if ((err = send_sig(sig, *p, 0)))
+				    retval = err;
+			}
+		}
+	}
+	else if (pid == -1) while (--p > &FIRST_TASK) {
 		if ((err = send_sig(sig,*p,0)))
 			retval = err;
 	} else while (--p > &FIRST_TASK)
 		if (*p && (*p)->pgrp == -pid)
 			if ((err = send_sig(sig,*p,0)))
 				retval = err;
-	return retval;
+	
+		return retval;
 }
 
-static void tell_father(int pid)
-{
+static void tell_father(int pid) {
 	int i;
 
 	if (pid)
@@ -198,4 +205,3 @@ repeat:
 	}
 	return -ECHILD;
 }
-
